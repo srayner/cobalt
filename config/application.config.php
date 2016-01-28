112df3a087772fb31807811268f121fd2574956a
Steve Rayner <srayner02@gmail.com>
2016-01-28 09:58:49 +0000

    dissabled civauth in config.

--- config/application.config.php
+++ config/application.config.php
@@ -11,7 +11,8 @@ return array(
      //   'ZfcUser',
         'TwbBundle',
         'Cobalt',
-        'Project'
+        'Project',
+     //   'CivAuth',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
