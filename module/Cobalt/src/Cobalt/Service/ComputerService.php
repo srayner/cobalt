5f4d5d44902326c2b73d9e25e89c8e66d3272326
Steve Rayner <srayner02@gmail.com>
2016-01-29 16:58:07 +0000

    Started building new computer service.

---
+++ module/Cobalt/src/Cobalt/Service/ComputerService.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Cobalt\Service;
+
+class ComputerService implements ComputerServiceInterface
+{
+    private $entityManager;
+    private $repository;
+    
+    public function __construct($entityManager, $repository)
+    {
+        $this->entityManager = $entityManager;
+        $this->repository = $repository;
+    }
+
+    public function count()
+    {
+        //todo
+        
+    }
+
+    public function findAll()
+    {
+        return $this->entityManager->getRepository($this->repository)->findAll();
+    }
+
+    public function findById($id)
+    {
+        return $this->entityManager->find($this->repository, $id);
+    }
+
+    public function findBySerial($serial)
+    {
+        return $this->entityManager->getRepository($this->repository)
+                                   ->findOneBy(array('serialNumber' => $serial));
+    }
+
+    public function persist($computer)
+    {
+        $this->entityManager->persist($computer);
+        $this->entityManager->flush();
+    }
+
+    public function remove($computer)
+    {
+        $this->entityManager->remove($computer);
+        $this->entityManager->flush(); 
+    }
+
+}
+
---
+++ module/Cobalt/src/Cobalt/Service/ComputerServiceInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Cobalt\Service;
+
+interface ComputerServiceInterface
+{
+    public function __construct($entityManager);
+    public function count();
+    public function findAll();
+    public function findById($id);
+    public function findBySerial($serial);
+    public function persist($computer);
+    public function remove($computer);
+}
