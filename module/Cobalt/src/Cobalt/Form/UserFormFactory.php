ba4247a293889118df130035be4f8b4b432ec63c
Steve Rayner <srayner02@gmail.com>
2016-02-05 13:30:38 +0000

    Removed old cobalt user model.

--- module/Cobalt/src/Cobalt/Form/UserFormFactory.php
+++ module/Cobalt/src/Cobalt/Form/UserFormFactory.php
@@ -2,17 +2,18 @@
 
 namespace Cobalt\Form;
 
-use Cobalt\Model\User\UserHydrator;
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
+use DoctrineModule\Stdlib\Hydrator\DoctrineObject as DoctrineHydrator;
 
 class UserFormFactory implements FactoryInterface
 {
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
+        $em = $serviceLocator->get('Doctrine\ORM\EntityManager');
         $form = new UserForm();
         $form->setInputFilter(new UserFilter());
-        $form->setHydrator(new UserHydrator());
+        $form->setHydrator(new DoctrineHydrator($em));
         return $form;
     }   
 }
