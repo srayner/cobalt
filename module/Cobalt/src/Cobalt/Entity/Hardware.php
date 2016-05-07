578f652c3f4bd7a7db337bcbbf64d52a1771a08b
Steve Rayner <srayner02@gmail.com>
2016-05-07 21:15:19 +0100

    Added hardware and printer entities.

---
+++ module/Cobalt/src/Cobalt/Entity/Hardware.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Cobalt\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\TableName="printer"
+ * @ORM\InheritanceType("JOINED")
+ * @ORM\DiscriminatorColumn(name="type", type="string")
+ */
+class Hardware
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */  
+    protected $id;
+    
+    /** @ORM\Column(type="string") */
+    protected $reference;
+    
+    /**
+     * @ORM\ManyToOne(targetEntity="HardwareType")
+     * @ORM\JoinColumn(name="type_id", referencedColumnName="id")
+     */
+    protected $type;
+    
+    /**
+     * @ORM\ManyToOne(targetEntity="HardwareStatus")
+     * @ORM\JoinColumn(name="status_id", referencedColumnName="id")
+     */
+    protected $status;
+    
+    /** @ORM\Column(type="string") */
+    protected $model;
+    
+    /** @ORM\Column(type="string", name="serial_number"*/
+    protected $serialNumber;
+    
+    /**
+     * @ORM\ManyToOne(targetEntity="HardwareManufacturer")
+     * @ORM\JoinColumn(name="manufacturer_id", referencedColumnName="id")
+     */
+    protected $manufacturer;
+    
+    /** @ORM\Column(type="date", name="purchase_date") */
+    protected $purchaseDate;
+    
+    /** @ORM\Column(type="date", name="warranty_end_date") */
+    protected $warrantyEndDate;
+    
+    /** @ORM\Column(type="date", name="disposal_date") */
+    protected $disposalDate;
+    
+    /** @ORM\Column(type="string", name="asset_number") */
+    protected $assetNumber;
+    
+    /** @ORM\Column(type="text") */
+    protected $notes;
+}
---
+++ module/Cobalt/src/Cobalt/Entity/Printer.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Cobalt\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\TableName="printer"
+ */
+class Printer extends Hardware
+{
+    /** @ORM\Column(type="string") */
+    protected $technology;
+    
+    /** @ORM\Column(type="string") */
+    protected $resolution;
+    
+    /** @ORM\Column(type="string") */
+    protected $speed;
+    
+    /** @ORM\Column(type="string") */
+    protected $quality;
+    
+    /** @ORM\Column(type="string", name="duty_cycle") */
+    protected $dutyCycle;
+    
+    public function getTechnology()
+    {
+        return $this->technology;
+    }
+
+    public function getResolution()
+    {
+        return $this->resolution;
+    }
+
+    public function getSpeed()
+    {
+        return $this->speed;
+    }
+
+    public function getQuality()
+    {
+        return $this->quality;
+    }
+
+    public function getDutyCycle()
+    {
+        return $this->dutyCycle;
+    }
+
+    public function setTechnology($technology)
+    {
+        $this->technology = $technology;
+        return $this;
+    }
+
+    public function setResolution($resolution)
+    {
+        $this->resolution = $resolution;
+        return $this;
+    }
+
+    public function setSpeed($speed)
+    {
+        $this->speed = $speed;
+        return $this;
+    }
+
+    public function setQuality($quality)
+    {
+        $this->quality = $quality;
+        return $this;
+    }
+
+    public function setDutyCycle($dutyCycle)
+    {
+        $this->dutyCycle = $dutyCycle;
+        return $this;
+    }
+}
