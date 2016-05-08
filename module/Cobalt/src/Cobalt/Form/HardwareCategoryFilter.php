27a199a957bb890db017c9b8cede3673fc8e16e0
Steve Rayner <srayner02@gmail.com>
2016-05-08 18:53:57 +0100

    Added hardware category form and filter.

---
+++ module/Cobalt/src/Cobalt/Form/HardwareCategoryFilter.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Cobalt\Form;
+
+use Zend\InputFilter\InputFilter;
+
+class HardwareCategoryFilter extends InputFilter
+{
+    public function __construct()
+    {
+        // name
+        $this->add(array(
+            'name'       => 'name',
+            'required'   => true,
+            'validators' => array(
+                array(
+                    'name'    => 'StringLength',
+                    'options' => array(
+                        'max' => 64,
+                    ),
+                ),
+            ),
+            'filters'   => array(
+                array('name' => 'StringTrim'),
+            ),
+        ));
+        
+        // description
+        $this->add(array(
+            'name'       => 'description',
+            'required'   => false,
+            'filters'   => array(
+                array('name' => 'StringTrim'),
+            ),
+        ));
+    }  
+}
+        
