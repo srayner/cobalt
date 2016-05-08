db6437b8592c6ef9cab851b8c00cfe3b4f6c5df8
Steve Rayner <srayner02@gmail.com>
2016-05-08 17:53:55 +0100

    Added hardware type form, and add action and view.

--- module/Cobalt/src/Cobalt/Controller/HardwareTypeController.php
+++ module/Cobalt/src/Cobalt/Controller/HardwareTypeController.php
@@ -15,4 +15,40 @@ class HardwareTypeController extends AbstractController
             'types' => $types
         ));
     }
+    
+    public function addAction()
+    {
+        // Create a new form.
+        $form = $this->getServiceLocator()->get('Cobalt\HardwareTypeForm');
+         
+        // Check if the request is a POST.
+        $request = $this->getRequest();
+        if ($request->isPost())
+        {
+            // POST, so check if valid.
+            $data = (array) $request->getPost();
+          
+            // Create a new company object.
+            $type = $this->getServiceLocator()->get('Cobalt\HardwareType');
+            
+            $form->bind($type);
+            $form->setData($data);
+            if ($form->isValid())
+            {
+          	// Persist type.
+            	$this->service->persist($type);
+                
+            	// Redirect to list of types
+		return $this->redirect()->toRoute('cobalt/default', array(
+		    'controller' => 'hardwaretype',
+                    'action'     => 'index'
+		));
+            }
+        } 
+        
+        // If not a POST request, or invalid data, then just render the form.
+        return new ViewModel(array(
+            'form'   => $form
+        ));
+    }
 }
--- module/Cobalt/src/Cobalt/Form/HardwareTypeFormFactory.php
+++ module/Cobalt/src/Cobalt/Form/HardwareTypeFormFactory.php
@@ -4,13 +4,16 @@ namespace Cobalt\Form;
 
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
+use DoctrineModule\Stdlib\Hydrator\DoctrineObject as DoctrineHydrator;
 
 class HardwareTypeFormFactory implements FactoryInterface
 {
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
+        $entityManager = $serviceLocator->get('Doctrine\ORM\EntityManager');
         $form = new HardwareTypeForm();
         $form->setInputFilter(new HardwareTypeFilter());
+        $form->setHydrator(new DoctrineHydrator($entityManager));
         return $form;
     }   
 }
