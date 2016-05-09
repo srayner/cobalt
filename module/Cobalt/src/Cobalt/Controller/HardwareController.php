6f173ea6dcbb7597df3a31d444523c132127f7df
Steve Rayner <srayner02@gmail.com>
2016-05-09 13:39:29 +0100

    Added hardware add action.

--- module/Cobalt/src/Cobalt/Controller/HardwareController.php
+++ module/Cobalt/src/Cobalt/Controller/HardwareController.php
@@ -15,4 +15,40 @@ class HardwareController extends AbstractController
             'hardware' => $hardware
         ));
     }
+    
+    public function addAction()
+    {
+        // Create a new form.
+        $form = $this->getServiceLocator()->get('Cobalt\HardwareForm');
+         
+        // Check if the request is a POST.
+        $request = $this->getRequest();
+        if ($request->isPost())
+        {
+            // POST, so check if valid.
+            $data = (array) $request->getPost();
+          
+            // Create a new hardware object.
+            $hardware = $this->getServiceLocator()->get('Cobalt\Hardware');
+            
+            $form->bind($hardware);
+            $form->setData($data);
+            if ($form->isValid())
+            {
+          	// Persist hardware.
+            	$this->service->persist($hardware);
+                
+            	// Redirect to list of hardware
+		return $this->redirect()->toRoute('cobalt/default', array(
+		    'controller' => 'hardware',
+                    'action'     => 'index'
+		));
+            }
+        } 
+        
+        // If not a POST request, or invalid data, then just render the form.
+        return new ViewModel(array(
+            'form'   => $form
+        ));
+    }
 }
