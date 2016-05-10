a4829599b9610a9b9b482dfacc48acdcfb46fe2c
Steve Rayner <srayner02@gmail.com>
2016-05-10 08:30:47 +0100

    Added hardware edit action and view.

--- module/Cobalt/src/Cobalt/Controller/HardwareController.php
+++ module/Cobalt/src/Cobalt/Controller/HardwareController.php
@@ -63,4 +63,57 @@ class HardwareController extends AbstractController
             'form'   => $form
         ));
     }
+    
+    public function editAction()
+    {
+        // Get a current copy of the entity.
+        $id = (int)$this->params()->fromRoute('id');
+        if (!$id) {
+            return $this->redirect()->toRoute('cobalt/default', array('controller' => 'hardware', 'action'=>'add'));
+	}
+        $hardware = $this->service->findById($id);
+        
+        // Create a new form instance and bind the entity to it.
+        $form = $this->getServiceLocator()->get('Cobalt\HardwareForm');
+        $form->bind($hardware);
+        
+        // Check if this request is a POST.
+        $request = $this->getRequest();
+        if ($request->isPost())
+        {
+            // Validate the data.
+            $form->setData($request->getPost());
+            if ($form->isValid())
+            {
+                // Save changes.
+                $this->service->persist($hardware);
+
+                // Redirect back to original referer.
+                return $this->redirect()->toUrl($this->retrieveReferer());
+            }
+        }
+        
+        $this->storeReferer('hardware/edit');
+        
+        return new ViewModel(array(
+            'id' => $id,
+            'form' => $form
+        ));
+    }
+    
+    private function storeReferer($except)
+    {
+        $referer = $this->getRequest()->getHeader('Referer')->uri()->getPath();
+        if (strpos($referer, $except) === false) {
+            $session = new Container('hardware');
+            $session->referer = $referer;
+        }
+    }
+    
+    private function retrieveReferer()
+    {
+        $session = new Container('hardware');
+        $referer = $session->referer;
+        return $referer;
+    }
 }
