73cea9e23d4f9a91b3c9360fac7e64656012f45a
Steve Rayner <srayner02@gmail.com>
2016-05-10 20:16:40 +0100

    Changed to printer to extend hardware form.

--- module/Cobalt/src/Cobalt/Form/PrinterForm.php
+++ module/Cobalt/src/Cobalt/Form/PrinterForm.php
@@ -2,20 +2,24 @@
 
 namespace Cobalt\Form;
 
-class PrinterForm extends HorizontalForm
+class PrinterForm extends HardwareForm
 {
     protected $em;
     
     public function __construct($em)
     {
-        parent::__construct();
+        parent::__construct($em);
         
-        $this->em = $em;
+        
         $this->compact = true;
         $this->labelWidth = 4;
         $this->controlWidth = 8;
         
         $this->addText('technology', 'Technology')
+             ->addText('resolution', 'Resolution')
+             ->addText('speed', 'Speed')
+             ->addText('quality', 'Quality')
+             ->addText('dutyCycle', 'Duty Cycle')
              ->addButton('submit', 'Add', 'btn-primary');   
     }
 }
