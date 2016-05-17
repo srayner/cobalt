781799d21eb9a36de10f028ff759d5ca6b626cec
Steve Rayner <srayner02@gmail.com>
2016-05-17 22:14:03 +0100

    Added support ticket impact add action and view.

--- module/Cobalt/src/Cobalt/Controller/TicketImpactController.php
+++ module/Cobalt/src/Cobalt/Controller/TicketImpactController.php
@@ -12,4 +12,40 @@ class TicketImpactController extends AbstractController
             'impacts' => $this->service->findAll()
         ));
     }
+    
+    public function addAction()
+    {
+        // Create a new form.
+        $form = $this->getServiceLocator()->get('Cobalt\TicketImpactForm');
+         
+        // Check if the request is a POST.
+        $request = $this->getRequest();
+        if ($request->isPost())
+        {
+            // POST, so check if valid.
+            $data = (array) $request->getPost();
+          
+            // Create a new hardware object.
+            $impact = $this->getServiceLocator()->get('Cobalt\TicketImpact');
+            
+            $form->bind($impact);
+            $form->setData($data);
+            if ($form->isValid())
+            {
+          	// Persist ticket impact.
+            	$this->service->persist($impact);
+                
+            	// Redirect to list of ticket states
+		return $this->redirect()->toRoute('cobalt/default', array(
+		    'controller' => 'ticketimpact',
+                    'action'     => 'index'
+		));
+            }
+        } 
+        
+        // If not a POST request, or invalid data, then just render the form.
+        return new ViewModel(array(
+            'form'   => $form
+        ));
+    }
 }
