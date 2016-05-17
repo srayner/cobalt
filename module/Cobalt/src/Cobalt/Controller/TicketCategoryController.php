b75711a3e917f1700e39095ed1e9038cd39e1395
Steve Rayner <srayner02@gmail.com>
2016-05-17 22:25:26 +0100

    Added helper functions to ticket category and ticket priority controllers.

--- module/Cobalt/src/Cobalt/Controller/TicketCategoryController.php
+++ module/Cobalt/src/Cobalt/Controller/TicketCategoryController.php
@@ -3,6 +3,7 @@
 namespace Cobalt\Controller;
 
 use Zend\View\Model\ViewModel;
+use Zend\Session\Container;
 
 class TicketCategoryController extends AbstractController
 {
@@ -11,5 +12,21 @@ class TicketCategoryController extends AbstractController
         return new ViewModel(array(
             'categories' => $this->service->findAll()
         ));
+    }
+    
+    private function storeReferer($except)
+    {
+        $referer = $this->getRequest()->getHeader('Referer')->uri()->getPath();
+        if (strpos($referer, $except) === false) {
+            $session = new Container('ticketcategory');
+            $session->referer = $referer;
+        }
+    }
+    
+    private function retrieveReferer()
+    {
+        $session = new Container('ticketcategory');
+        $referer = $session->referer;
+        return $referer;
     }
 }
--- module/Cobalt/src/Cobalt/Controller/TicketPriorityController.php
+++ module/Cobalt/src/Cobalt/Controller/TicketPriorityController.php
@@ -3,6 +3,7 @@
 namespace Cobalt\Controller;
 
 use Zend\View\Model\ViewModel;
+use Zend\Session\Container;
 
 class TicketPriorityController extends AbstractController
 {
@@ -11,5 +12,21 @@ class TicketPriorityController extends AbstractController
         return new ViewModel(array(
             'priorities' => $this->service->findAll()
         ));
+    }
+    
+    private function storeReferer($except)
+    {
+        $referer = $this->getRequest()->getHeader('Referer')->uri()->getPath();
+        if (strpos($referer, $except) === false) {
+            $session = new Container('ticketpriority');
+            $session->referer = $referer;
+        }
+    }
+    
+    private function retrieveReferer()
+    {
+        $session = new Container('ticketpriority');
+        $referer = $session->referer;
+        return $referer;
     }
 }
