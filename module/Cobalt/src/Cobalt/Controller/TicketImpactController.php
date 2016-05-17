6aaae1fe8bfd3057724231a8da8db37ad1b8e302
Steve Rayner <srayner02@gmail.com>
2016-05-17 22:18:18 +0100

    Added support ticket impact edit action and view.

--- module/Cobalt/src/Cobalt/Controller/TicketImpactController.php
+++ module/Cobalt/src/Cobalt/Controller/TicketImpactController.php
@@ -3,6 +3,7 @@
 namespace Cobalt\Controller;
 
 use Zend\View\Model\ViewModel;
+use Zend\Session\Container;
 
 class TicketImpactController extends AbstractController
 {
@@ -47,5 +48,70 @@ class TicketImpactController extends AbstractController
         return new ViewModel(array(
             'form'   => $form
         ));
+    }
+    
+    public function editAction()
+    {
+        // Ensure we have an id, else redirect to add action.
+        $id = (int) $this->params()->fromRoute('id', 0);
+        if (!$id) {
+             return $this->redirect()->toRoute('cobalt/default', array(
+                 'controller' => 'ticketimpact',
+                 'action' => 'add'
+             ));
+        }
+        
+        // Grab the impact with the specified id.
+        $impact = $this->service->findById($id);
+        
+        $form = $this->getServiceLocator()->get('Cobalt\TicketImpactForm');
+        $form->bind($impact);
+        $form->get('submit')->setAttribute('value', 'Edit');
+        
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+        
+            $form->setData($request->getPost());
+            if ($form->isValid()) {
+                
+                // Persist impact.
+            	$this->service->persist($impact);
+                
+                // Redirect to original referer
+                return $this->redirect()->toUrl($this->retrieveReferer());
+            }     
+        }
+        
+        $this->storeReferer('ticketimpact/edit');
+        
+        return new ViewModel(array(
+             'id' => $id,
+             'form' => $form,
+        ));
+    }
+    
+    public function detailAction()
+    {
+        $id = (int) $this->params()->fromRoute('id', 0);
+        $impact = $this->service->findById($id);
+        return new ViewModel(array(
+            'impact' => $impact
+        ));    
+    }
+    
+    private function storeReferer($except)
+    {
+        $referer = $this->getRequest()->getHeader('Referer')->uri()->getPath();
+        if (strpos($referer, $except) === false) {
+            $session = new Container('ticketimpact');
+            $session->referer = $referer;
+        }
+    }
+    
+    private function retrieveReferer()
+    {
+        $session = new Container('ticketimpact');
+        $referer = $session->referer;
+        return $referer;
     }
 }
