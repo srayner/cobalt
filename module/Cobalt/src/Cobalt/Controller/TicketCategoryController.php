635cc632064cf907c4d560b85979a577410288fc
Steve Rayner <srayner02@gmail.com>
2016-05-18 08:26:37 +0100

    Added ticket category add and edit actions and views.

--- module/Cobalt/src/Cobalt/Controller/TicketCategoryController.php
+++ module/Cobalt/src/Cobalt/Controller/TicketCategoryController.php
@@ -14,6 +14,82 @@ class TicketCategoryController extends AbstractController
         ));
     }
     
+    public function addAction()
+    {
+        // Create a new form.
+        $form = $this->getServiceLocator()->get('Cobalt\TicketCategoryForm');
+         
+        // Check if the request is a POST.
+        $request = $this->getRequest();
+        if ($request->isPost())
+        {
+            // POST, so check if valid.
+            $data = (array) $request->getPost();
+          
+            // Create a new hardware object.
+            $category = $this->getServiceLocator()->get('Cobalt\TicketCategory');
+            
+            $form->bind($category);
+            $form->setData($data);
+            if ($form->isValid())
+            {
+          	// Persist ticket status.
+            	$this->service->persist($category);
+                
+            	// Redirect to list of ticket states
+		return $this->redirect()->toRoute('cobalt/default', array(
+		    'controller' => 'ticketcategory',
+                    'action'     => 'index'
+		));
+            }
+        }
+        
+        // If not a POST request, or invalid data, then just render the form.
+        return new ViewModel(array(
+            'form'   => $form
+        ));
+    }
+    
+    public function editAction()
+    {
+        // Ensure we have an id, else redirect to add action.
+        $id = (int) $this->params()->fromRoute('id', 0);
+        if (!$id) {
+             return $this->redirect()->toRoute('cobalt/default', array(
+                 'controller' => 'ticketcategory',
+                 'action' => 'add'
+             ));
+        }
+        
+        // Grab the category with the specified id.
+        $category = $this->service->findById($id);
+        
+        $form = $this->getServiceLocator()->get('Cobalt\TicketCategoryForm');
+        $form->bind($category);
+        $form->get('submit')->setAttribute('value', 'Edit');
+        
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+        
+            $form->setData($request->getPost());
+            if ($form->isValid()) {
+                
+                // Persist category.
+            	$this->service->persist($category);
+                
+                // Redirect to original referer
+                return $this->redirect()->toUrl($this->retrieveReferer());
+            }     
+        }
+        
+        $this->storeReferer('ticketcategory/edit');
+        
+        return new ViewModel(array(
+             'id' => $id,
+             'form' => $form,
+        ));
+    }
+    
     private function storeReferer($except)
     {
         $referer = $this->getRequest()->getHeader('Referer')->uri()->getPath();
