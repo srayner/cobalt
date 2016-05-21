49affd2514d8f75a9e2c03b8be86923ff36ea941
Steve Rayner <srayner02@gmail.com>
2016-05-21 20:10:42 +0100

    Added domain and password fields to user db schema.

--- data/db_schema/mysql/organisation.sql
+++ data/db_schema/mysql/organisation.sql
@@ -38,6 +38,8 @@ CREATE TABLE department(
 CREATE TABLE `user` (
   id                  Integer NOT NULL AUTO_INCREMENT,
   username            VarChar(128),
+  password            VarChar(128),
+  domain              Varchar(128),
   email_address       VarChar(256),
   sam_account_name    VarChar(128),
   user_principal_name VarChar(128),
