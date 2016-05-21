db9b3cfefcf90e82ee311245a0fec2a9c87050ad
Steve Rayner <srayner02@gmail.com>
2016-05-21 20:27:31 +0100

    Tidied application.config.php file.

--- config/application.config.php
+++ config/application.config.php
@@ -1,37 +1,28 @@
 <?php
 return array(
     
-    // Module to load in the application.
+    // Modules to load in the application.
     'modules' => array(
         'CivUser',
-        'Application',
-   //     'ZendDeveloperTools',
         'DoctrineModule',
         'DoctrineORMModule',
-        'ZfcBase',
-        
         'TwbBundle',
+        'Application',
         'Cobalt',
         'FAQ',
         'Project',
-        
-     //   'CivAuth',
     ),
 
-    // These are various options for the listeners attached to the ModuleManager
     'module_listener_options' => array(
-        // This should be an array of paths in which modules reside.
-        // If a string key is provided, the listener will consider that a module
-        // namespace, the value of that key the specific path to that module's
-        // Module class.
+        
+        // Paths to look for modules.
         'module_paths' => array(
             './module',
             './vendor',
         ),
 
-        // An array of paths from which to glob configuration files after
-        // modules are loaded. These effectively override configuration
-        // provided by modules themselves. Paths may use GLOB_BRACE notation.
+        // Paths from which to glob configuration files after modules are
+        // loaded. These override configuration provided by modules themselves.
         'config_glob_paths' => array(
             'config/autoload/{,*.}{global,local}.php',
         ),
