d679f8399ea5f2c7a395c2c51243cf78a36851c3
Steve Rayner <srayner02@gmail.com>
2016-06-02 17:01:47 +0100

    Added html template.

--- module/Cobalt/src/Cobalt/Service/TicketService.php
+++ module/Cobalt/src/Cobalt/Service/TicketService.php
@@ -27,7 +27,7 @@ class TicketService extends AbstractEntityService
         $mail = $this->mailService->createMail('Support Ticket Raised', $fromName, $fromAddress);
         $this->mailService->addParticipant($mail, 'to', $to->getDisplayName(), $to->getEmailAddress());
         $this->mailService->addParticipant($mail, 'from', $fromName, $fromAddress);
-        $this->mailService->addContent($mail, 'text/plain', $this->renderTemplate($ticket));
+        $this->mailService->addContent($mail, 'text/html', $this->renderTemplate($ticket));
         $this->mailService->persist($mail);
     }
     
@@ -41,7 +41,7 @@ class TicketService extends AbstractEntityService
     private function renderTemplate($ticket)
     {
         $loader = new \Twig_Loader_Array(array(
-            'new_ticket' => $this->getTemplate('new_ticket')
+            'new_ticket' => $this->getTemplate('new_ticket_html')
         ));
         $environment = new \Twig_Environment($loader);
         $txt = $environment->render('new_ticket', array(
