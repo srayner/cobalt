10bb3a7d8ec0ed9d1b49aaf287f00f24b3623cae
Steve Rayner <srayner02@gmail.com>
2016-06-02 13:41:20 +0100

    Support ticket email now uses a twig template.

--- module/Cobalt/src/Cobalt/Service/TicketService.php
+++ module/Cobalt/src/Cobalt/Service/TicketService.php
@@ -27,8 +27,34 @@ class TicketService extends AbstractEntityService
         $mail = $this->mailService->createMail('Support Ticket Raised', $fromName, $fromAddress);
         $this->mailService->addParticipant($mail, 'to', $to->getDisplayName(), $to->getEmailAddress());
         $this->mailService->addParticipant($mail, 'from', $fromName, $fromAddress);
-        $this->mailService->addContent($mail, 'text/plain', 'A new support ticket has been raised.');
+        $this->mailService->addContent($mail, 'text/plain', $this->renderTemplate($ticket));
         $this->mailService->persist($mail);
+    }
+    
+    // Returns a template of the given name.
+    // This can be refactored later.
+    private function getTemplate($name)
+    {
+        return file_get_contents("data\\templates\\$name");
+    }
+    
+    private function renderTemplate($ticket)
+    {
+        $loader = new \Twig_Loader_Array(array(
+            'new_ticket' => $this->getTemplate('new_ticket')
+        ));
+        $environment = new \Twig_Environment($loader);
+        $txt = $environment->render('new_ticket', array(
+            'RequesterDisplayName'  => $ticket->getRequestor()->getDisplayName(),
+            'TechnicianDisplayName' => $ticket->getTechnician()->getDisplayName(),
+            'TicketId'              => $ticket->getId(),
+            'TicketSubject'         => $ticket->getSubject(),
+            'TicketStatus'          => $ticket->getStatus()->getName(),
+            'TicketPriority'        => $ticket->getPriority()->getName(),
+            'TicketProblem'         => $ticket->getProblem(),
+            'TicketResolutionDue'   => $ticket->getResolutionDue()->format('d/m/Y'),
+        ));
+        return $txt;
     }
     
 }
