1c79d73540d5de10c030bca678432bcf9fde8567
Steve Rayner <srayner02@gmail.com>
2016-06-05 18:11:36 +0100

    added second notification template to database.

--- data/db_schema/mysql/notification.sql
+++ data/db_schema/mysql/notification.sql
@@ -9,7 +9,7 @@ create table notification_template(
 ) ENGINE=InnoDb DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 insert into notification_template(id, name, description, mime_type, content)
-values(1, 'new_ticket', 'New Support Ticket Raised.', 'text/html',
+values(1, 'requestor_new_ticket', 'Notify requestor that a new support ticket has been raised.', 'text/html',
 'Dear {{ RequesterDisplayName }},
 
 This is an acknowledgement that your support ticket has been logged with the following details;
@@ -26,6 +26,26 @@ Expected Resolution Date: {{ TicketResolutionDue }}
 
 Please reply to this email if you have any further clarifications.
 
+
+Regards,
+
+The I.C.T. Department.');
+
+insert into notification_template(id, name, description, mime_type, content)
+values(2, 'technician_new_ticket', 'Notify technician that a new support ticket has been raised.', 'text/html',
+'Dear {{ TechnicianDisplayName }},
+
+This is an acknowledgement that a support ticket has been assigned to you with the following details;
+
+Support Ticket Number: {{ TicketId }}.
+
+Title: {{ TicketSubject }}
+Status: {{ TicketStatus }}
+Assigned Technician: {{ TechnicianDisplayName }} 
+Expected Resolution Date: {{ TicketResolutionDue }} 
+
+
+{{ TicketProblem }}
 
 Regards,
 
