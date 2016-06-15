f73a5d808d0a6b365f50f154c831f1a067cb730c
Steve Rayner <srayner02@gmail.com>
2016-06-15 19:58:13 +0100

    Added maps to company detail.

--- public/js/cobalt/company/detail.js
+++ public/js/cobalt/company/detail.js
@@ -7,3 +7,26 @@ $(document).ready(function() {
     
     $('#tabs').stickyTabs();
 } );
+
+function initialize() {
+
+    var map = new google.maps.Map(document.getElementById("map"), {zoom: 15});
+    var geocoder = new google.maps.Geocoder();
+    var address = $('#address').text();
+    
+    geocoder.geocode( { 'address': address}, function(results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+            map.setCenter(results[0].geometry.location);
+            console.log('Google Lat: ' + results[0].geometry.location);
+            console.log('Google Lng: ' + results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: map, 
+                position: results[0].geometry.location
+            });
+        } else {
+            alert("Geocode was not successful for the following reason: " + status);
+        }
+    });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
