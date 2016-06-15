7d44c8d98a08bda1081e40223f788f0bc8a29676
Steve Rayner <srayner02@gmail.com>
2016-06-15 19:54:31 +0100

    Removed obsolete code from office detail.js

--- public/js/cobalt/office/detail.js
+++ public/js/cobalt/office/detail.js
@@ -28,16 +28,3 @@ function initialize() {
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
-
-
-//function initialize() {
-//    var latlng = new google.maps.LatLng(-34.397, 150.644);
-//    var myOptions = {
-//        zoom: 8,
-//        center: latlng,
-//        mapTypeId: google.maps.MapTypeId.ROADMAP
-//    };
-//    var map = new google.maps.Map(document.getElementById("map"),
-//            myOptions);
-//}
-//google.maps.event.addDomListener(window, "load", initialize);
