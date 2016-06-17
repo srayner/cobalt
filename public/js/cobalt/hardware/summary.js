f131b236e6539107fbc6b740018d91d59e2934d6
Steve Rayner <srayner02@gmail.com>
2016-06-17 21:10:25 +0100

    Added demo chart.

---
+++ public/js/cobalt/hardware/summary.js
@@ -0,0 +1,16 @@
+
+
+
+window.onload=function(){  // Render Method[2]
+    var data = {
+      series: [5, 3, 4]
+    };
+
+    var sum = function(a, b) { return a + b };
+
+    new Chartist.Pie('.ct-chart', data, {
+        labelInterpolationFnc: function(value) {
+            return Math.round(value / data.series.reduce(sum) * 100) + '%';
+        }
+    });
+};
