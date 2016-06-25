74edeb2cb701469a6386968bec3ab03946eed11a
Steve Rayner <srayner02@gmail.com>
2016-06-25 08:32:35 +0100

    Added Json view strategy.

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -106,6 +106,8 @@ return array(
             'Application\Controller\Index' => 'Application\Controller\IndexController'
         ),
     ),
+    
+    // View manager config.
     'view_manager' => array(
         'display_not_found_reason' => true,
         'display_exceptions'       => true,
@@ -122,7 +124,11 @@ return array(
         'template_path_stack' => array(
             __DIR__ . '/../view',
         ),
+        'strategies' => array(
+            'ViewJsonStrategy',
+        ),
     ),
+    
     // Placeholder for console routes
     'console' => array(
         'router' => array(
