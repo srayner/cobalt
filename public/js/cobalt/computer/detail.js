86871dda3b44358303c32c11c32711a36a81dec9
Steve Rayner <srayner02@gmail.com>
2016-06-25 09:38:20 +0100

    Added ajax request to ping computer and display result as a badge.

--- public/js/cobalt/computer/detail.js
+++ public/js/cobalt/computer/detail.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
 
     $.get("/computer/ping/4", function(data, status){
-        alert("result: " + data.status);
+        //alert("result: " + data.status);
+        data.class='label label-default';
+        data.text = 'Offline';
+        if (data.status === 'alive') {
+            data.text = 'Online';
+            data.class = 'label label-success';
+        }
+        $('#status').text(data.text)
+                    .addClass(data.class);
     });
     
 });
