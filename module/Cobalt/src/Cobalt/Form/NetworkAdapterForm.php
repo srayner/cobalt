bec5d18e04b1ef7cb2260b785120bfa2cf5665de
Steve Rayner <srayner02@gmail.com>
2016-07-01 22:32:58 +0100

    Added network adapter form.

---
+++ module/Cobalt/src/Cobalt/Form/NetworkAdapterForm.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Cobalt\Form;
+
+class NetworkAdapterForm extends HorizontalForm
+{
+    public function __construct()
+    {
+        parent::__construct();
+        
+        $this->compact = true;
+        $this->labelWidth = 4;
+        $this->controlWidth = 8;
+        
+        $this->addText('name', 'Name')
+             ->addText('dnsSuffix', 'DNS Suffix')
+             ->addText('ipv6Address', 'IPv6 Address')
+             ->addText('tempIpv6Address', 'Temp IPv6 Address')
+             ->addText('localIpv6Address', 'Local IPv6 Address')
+             ->addText('physicalAddress', 'Physical Address')
+             ->addText('ipv4Address', 'IPv4 Address')
+             ->addText('networkMask', 'Network Mask')
+             ->addText('subnetMask', 'Subnet Mask')
+             ->addText('defaultGateway', 'Default Gateway')
+             ->addText('dhcpEnabled', 'DHCP Enabled')
+             ->addText('preferredDnsServer', 'Preferred DNS Server')
+             ->addText('alternameDnsServer', 'Alternate DNS Server')
+             ->addButton('submit', 'Add', 'btn-primary');
+    }
+}
