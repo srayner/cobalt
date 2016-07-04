f5c53b11842ff9a5c315182b5f5652741713364d
Steve Rayner <srayner02@gmail.com>
2016-07-04 16:57:50 +0100

    Added custom doctrine repository for user entities.

---
+++ module/Cobalt/src/Cobalt/Repository/User.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Cobalt\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+class User extends EntityRepository
+{
+    public function getTechnicians()
+    {
+        
+    }   
+}
