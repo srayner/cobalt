6cb70d877a74008ed38263ee0ae49ac82d9816e7
Steve Rayner <srayner02@gmail.com>
2016-07-04 13:50:09 +0100

    Added ping capability to network adapter service.

--- module/Cobalt/src/Cobalt/Service/NetworkAdapterService.php
+++ module/Cobalt/src/Cobalt/Service/NetworkAdapterService.php
@@ -4,5 +4,28 @@ namespace Cobalt\Service;
 
 class NetworkAdapterService extends AbstractEntityService
 {
+    public function pingAndUpdate($id)
+    {
+        $adapter = $this->findById($id);
+        if ($adapter->getIpv4())
+        {
+            $host = $adapter->getIpv4();
+            $status = $this->ping($host);
+            if ($status == 'alive') {
+                $adapter->getHardware()->setLastSeen(New Date);
+            }
+        }
+    }
+    
+    private function ping($host)
+    {
+        exec("ping -n 1 -w 1000 $host", $output, $status);
+        if (0 == $status) {
+            $status = "alive";
+        } else {
+            $status = "dead";
+        }
+        return $status;
+    }
     
 }
