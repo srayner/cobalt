eaa006c4e4cc9b699a28cdb915e42b17d5bebf24
Steve Rayner <srayner02@gmail.com>
2016-07-11 13:31:14 +0100

    Added consumable doctrine entity.

---
+++ module/Cobalt/src/Cobalt/Entity/Consumable.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Cobalt\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/** @ORM\Entity
+  * @ORM\Table(name="consumable")
+  */
+class Consumable
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */  
+    protected $id;
+    
+    /** @ORM\Column(type="string", name="consumable_name") */
+    protected $name;
+    
+    /** @ORM\Column(type="text", name="description") */
+    protected $description;
+    
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function setDescription($description)
+    {
+        $this->description = $description;
+        return $this;
+    }
+}
