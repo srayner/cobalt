c93739cae6b5b54fbba44cb12770675714573dd6
Steve Rayner <srayner02@gmail.com>
2016-08-19 21:02:11 +0100

    Added are you sure to ticket add/edit scripts.

--- public/js/cobalt/ticket/add.js
+++ public/js/cobalt/ticket/add.js
@@ -1,10 +1,17 @@
 $(document).ready(function() {
+    $('form').areYouSure();
     tinymce.init({
         selector:'textarea',
         plugins: 'textcolor',
         content_css : "/css/editor_content.css",
         statusbar: false,
         menubar: false,
+        setup: function(editor) {
+            editor.on('dirty', function(event) {
+            editor.save();
+            $('form').trigger('checkform.areYouSure');
+            });
+        },
         toolbar: 'undo redo | styleselect | bold italic | alignleft | alignright | aligncenter alignjustify | bullist numlist | forecolor'
     });
 } );
