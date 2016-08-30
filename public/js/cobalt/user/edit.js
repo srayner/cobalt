04ccd7c95c9963db0ad24a1798f5f327acb18fb7
Steve Rayner <srayner02@gmail.com>
2016-08-30 11:08:02 +0100

    Updated user edit javascipt to make office select dynamic.

--- public/js/cobalt/user/edit.js
+++ public/js/cobalt/user/edit.js
@@ -1,3 +1,11 @@
+function updateSelects(data)
+{
+    $("select[name='office']").find('option').remove();
+    data['offices'].forEach(function(item, index){
+       $("select[name='office']").append('<option value="' + item.id + '">' + item.name + '</option>');
+    });
+}
+
 $(document).ready(function() {
   
     var input = $("input[name='username']");
@@ -5,6 +13,17 @@ $(document).ready(function() {
     input[0].focus();
     input[0].setSelectionRange(len, len);
 
+    $("select[name='company'").change(function(e) {
+        var companyId = this.options[e.target.selectedIndex].value;
+        $.ajax({
+            url: '/company/offices/' + companyId,
+            success: updateSelects,
+            dataType: 'json'
+        });
+    });
+
     $('form').areYouSure();
     
 });
+
+
